ff0551cab98f47746d4f6f55703f77759803f69d
Curtis Patrick <curtis.patrick84@gmail.com>
2018-09-04 15:33:34 -0700

    Adding morgan-logging, /pxeconfig endpoint

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const fs = require('fs')
 
 const app = express()
 app.use(bodyParser.json({ type: 'application/json' }))
+app.use(morgan('combined'))
+
+const PXECFG = '/var/tftpboot/pxelinux.cfg'
 
 app.get('/test', (req, res) => {
   res.json({ oh: 'hai' })
+})
+
+app.put('/pxeconfig', ({ body: { mac, os }}, res) => {
+  if (host && mac) {
+    parsedMacAddress = `01-${mac.replace(':', '-')}`
+    // first, remove the old symlink
+    fs.unlink(`${PXECFG}/${parsedMacAddress}`, err => {})
+    // next, link the pxe file to the request OS recipe
+    fs.link(`${PXECFG}/${os}.cfg`, `${PXECFG}/${parsedMacAddress}`, err => {})
+
+    res.json({ status: 'success' })
+  }
+  res.sendStatus(422)
 })
 
 app.listen(8069, () => console.log('At your service...'))
